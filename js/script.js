dcd696f3c3d05e92a622c90f0cf917b9c4e031f1
Pierre Gronnier <pierre.gronnier@gmail.com>
2024-11-25 15:49:58 +0100

    change style and add function gridChange

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
 let gridContainer = document.querySelector(".gridContainer");
+let gridSize = document.querySelector("#gridSize")
 let grid = 16 * 16;
 
-for (let i = 1; i <= grid; i++){
-    const gridItem = document.createElement("div");
-    gridItem.classList.add("gridItem")
-    gridItem.style.flexBasis = "calc(100% / 16)";
-    gridItem.addEventListener("mouseover", () => {
-        gridItem.style.backgroundColor = "black"
-    })
-    gridContainer.appendChild(gridItem)
+function createGrid(){
+    gridContainer.innerHTML = "";
+    for (let i = 1; i <= grid; i++){
+        const gridItem = document.createElement("div");
+        gridItem.classList.add("gridItem")
+        gridItem.style.flexBasis = `calc(100% / ${Math.sqrt(grid)})`;
+        gridItem.addEventListener("mouseover", () => {
+            gridItem.style.backgroundColor = "black"
+        })
+        gridContainer.appendChild(gridItem)
+    }
 }
 
+function gridChange(){
+    gridSize.addEventListener("click", () => {
+        const newSize = prompt("Enter the number of squares per side (between 1 and 100)")
+        if (newSize > 100 || newSize < 1){
+            alert("Please enter a number between 1 and 100")
+        }
+        else {
+            grid = newSize * newSize
+            createGrid()
+        }
+    })
+}
 
+createGrid();
+gridChange();
