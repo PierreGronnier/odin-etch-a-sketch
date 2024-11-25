ecd7cc4b55cdb9500de22ffd2390aaea3de0e1a1
Pierre Gronnier <pierre.gronnier@gmail.com>
2024-11-25 14:30:15 +0100

    add color when hover

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 let gridContainer = document.querySelector(".gridContainer");
-let grid = 256;
+let grid = 16 * 16;
 
 for (let i = 1; i <= grid; i++){
     const gridItem = document.createElement("div");
     gridItem.classList.add("gridItem")
-    gridItem.textContent = i;
+    gridItem.style.flexBasis = "calc(100% / 16)";
+    gridItem.addEventListener("mouseover", () => {
+        gridItem.style.backgroundColor = "black"
+    })
     gridContainer.appendChild(gridItem)
 }
 
